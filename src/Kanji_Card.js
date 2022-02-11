9b38c2539c128b62a95dbc5553710e8564d9a4e7
Jalastin <justin_rusit@yahoo.com>
2022-02-10 19:27:18 -0800

    added support for katakana/romaji

--- src/Kanji_Card.js
+++ src/Kanji_Card.js
@@ -11,7 +11,6 @@ const Kanji_Card = ({id}) => {
     const [loading, setLoading] = React.useState(true);
     const [kanji, setKanji] = React.useState([]);
     const [kanjiId, setKanjiId] = React.useState([]);
-    const [correct, setCorrect] = React.useState("Input the correct pronunciation here!");
 
     useEffect(() => {
         const unsubscribe = onSnapshot(doc(dbConfig, "kanji-site", id), (doc) => {
@@ -20,11 +19,6 @@ const Kanji_Card = ({id}) => {
                 setKanji(doc.data());
                 setKanjiId(doc.id);
                 // https://stackoverflow.com/questions/40950546/react-js-right-way-to-iterate-over-object-instead-of-object-entries
-                // console.log(doc.data().word);
-                // Object.entries(doc.data().word).map(([key,value]) => {
-                //     console.log(key);
-                //     console.log(value);
-                // })
             },
             err => {
                 setError(err);
@@ -33,28 +27,11 @@ const Kanji_Card = ({id}) => {
         return () => unsubscribe()
     }, [id])
 
-    // https://stackoverflow.com/questions/31272207/to-call-onchange-event-after-pressing-enter-key
-    // https://thewebdev.info/2021/09/25/how-to-get-input-text-field-values-when-enter-key-is-pressed-in-react/
-    // https://stackoverflow.com/questions/37639122/using-event-target-with-react-components
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-          if (event.target.value == event.target.dataset.pronunciation) {
-              console.log("user input: " + event.target.value);
-              console.log("correct pronunciation: " + event.target.dataset.pronunciation);
-              setCorrect("You were Correct!");
-          } else {
-            console.log("user input: " + event.target.value);
-            console.log("correct pronunciation: " + event.target.dataset.pronunciation);
-            setCorrect("You were Wrong.");
-          }
-        }
-      }
-
     return (
         <div>
             <div>Kanji: {kanjiId}</div>
-            <div>Stroke Order: {kanji.stroke_number}</div>
-            <div>JLPT: {kanji.jlpt}</div>
+            {/* <div>Stroke Order: {kanji.stroke_number}</div>
+            <div>JLPT: {kanji.jlpt}</div> */}
             {typeof(kanji.word) !== 'undefined' && kanji.word != null ? Object.entries(kanji.word).map(([key,value]) => {
                     return (
                         <Word_Section word={key} pronunciation={value.pronunciation}/>
--- src/Word_Section.js
+++ src/Word_Section.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { isKatakana, isRomaji, toHiragana } from "wanakana";
 
 const Word_Section = ({word, pronunciation}) => {
     const [correct, setCorrect] = React.useState("Input the correct pronunciation here!");
@@ -6,14 +7,20 @@ const Word_Section = ({word, pronunciation}) => {
     // https://thewebdev.info/2021/09/25/how-to-get-input-text-field-values-when-enter-key-is-pressed-in-react/
     // https://stackoverflow.com/questions/37639122/using-event-target-with-react-components
     const handleKeyDown = (event) => {
+        var v = event.target.value;
+        const p = event.target.dataset.pronunciation;
+        if (isKatakana(v) || isRomaji(v)) {
+            v = toHiragana(v);
+        }
+
         if (event.key === 'Enter') {
-          if (event.target.value == event.target.dataset.pronunciation) {
-              console.log("user input: " + event.target.value);
-              console.log("correct pronunciation: " + event.target.dataset.pronunciation);
+          if (v == p) {
+              console.log("user input: " + v);
+              console.log("correct pronunciation: " + p);
               setCorrect("You were Correct!");
           } else {
-            console.log("user input: " + event.target.value);
-            console.log("correct pronunciation: " + event.target.dataset.pronunciation);
+            console.log("user input: " + v);
+            console.log("correct pronunciation: " + p);
             setCorrect("You were Wrong.");
           }
         }
